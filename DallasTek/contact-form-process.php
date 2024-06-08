827415a4b4c5527f1bc681eed0fafc475ba15e01
mada063 <98733773+mada063@users.noreply.github.com>
2024-06-08 19:27:42 +0200

    Add files via upload

--- DallasTek/contact-form-process.php
+++ DallasTek/contact-form-process.php
@@ -1,5 +1,4 @@
 <?php
-// Define functions at the top
 function problem($error) {
     echo "We're sorry, but there were error(s) found with the form you submitted. ";
     echo "These errors appear below.<br><br>";
@@ -52,11 +51,12 @@ if (isset($_POST['Email'])) {
     $email_message .= "Message: " . clean_string($message) . "\n";
 
     // Creating email headers
-    $headers = 'From: ' . $email . "\r\n" .
+    $from_email = 'mathias@dallastek.no'; // Replace with your own domain email
+    $headers = 'From: ' . $from_email . "\r\n" .
                'Reply-To: ' . $email . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
 
-    // Send email and check for success
+    // Send email
     if(mail($email_to, $email_subject, $email_message, $headers)) {
         echo 'Takk for at du tok kontakt! Vi vil svare deg snart.';
     } else {
